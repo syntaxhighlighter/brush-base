178ebfdd2b9714d51579437c0381b33cad6853c0
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-12-13 11:41:51 -0800

    Updates to ES2015

--- brush-base.js
+++ brush-base.js
@@ -2,40 +2,37 @@ import XRegExp from 'xregexp';
 import Renderer from 'html-renderer';
 import parser from 'parser';
 
-function BrushBase()
-{
-}
-
-BrushBase.prototype = {
+module.exports = class BrushBase {
   /**
    * Converts space separated list of keywords into a regular expression string.
-   * @param {String} str    Space separated keywords.
-   * @return {String}       Returns regular expression string.
+   * @param {String} str Space separated keywords.
+   * @return {String} Returns regular expression string.
    */
-  getKeywords: function(str)
+  getKeywords(str)
   {
-    str = str
+    const results = str
       .replace(/^\s+|\s+$/g, '')
       .replace(/\s+/g, '|')
       ;
 
-    return '\\b(?:' + str + ')\\b';
-  },
+    return `\\b(?:${results})\\b`;
+  }
 
   /**
    * Makes a brush compatible with the `html-script` functionality.
    * @param {Object} regexGroup Object containing `left` and `right` regular expressions.
    */
-  forHtmlScript: function(regexGroup)
+  forHtmlScript(regexGroup)
   {
-    var regex = {'end': regexGroup.right.source};
+    const regex = { 'end': regexGroup.right.source };
 
-    if(regexGroup.eof)
-      regex.end = "(?:(?:" + regex.end + ")|$)";
+    if (regexGroup.eof) {
+      regex.end = `(?:(?:${regex.end})|$)`;
+    }
 
     this.htmlScript = {
-      left: {regex: regexGroup.left, css: 'script'},
-      right: {regex: regexGroup.right, css: 'script'},
+      left: { regex: regexGroup.left, css: 'script' },
+      right: { regex: regexGroup.right, css: 'script' },
       code: XRegExp(
         "(?<left>" + regexGroup.left.source + ")" +
         "(?<code>.*?)" +
@@ -43,13 +40,11 @@ BrushBase.prototype = {
         "sgi"
         )
     };
-  },
+  }
 
   getHtml(code, params = {}) {
     const matches = parser.parse(code, this.regexList, params);
     const renderer = new Renderer(code, matches, params);
     return renderer.getHtml();
-  },
+  }
 };
-
-module.exports = BrushBase;
